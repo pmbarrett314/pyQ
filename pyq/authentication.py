a35659db436a3d5e935565099e3049aaba98aa21
Paul Barrett <pmbarrett314@gmail.com>
2018-02-25 17:23:47 -0600

    Implemented authentication

---
+++ pyq/authentication.py
@@ -0,0 +1,46 @@
+import json
+import requests
+
+
+def authenticate(email, password, client_id, client_secret):
+    use_password = email is not None and password is not None
+    use_client_secret = not use_password and client_id is not None and client_secret is not None
+
+    if use_password:
+        token_dict = get_token_email_password(email, password)
+    elif use_client_secret:
+        token_dict = get_token_client_secret(client_id, client_secret)
+    else:
+        message = "Either an email/password combination or a client id/secret combination is needed"
+        raise NoAuthenticationMethodException(message)
+
+    return token_dict
+
+
+def get_token_email_password(email, password):
+    data = {
+        'email': email,
+        'password': password,
+        'grant_type': 'password'
+    }
+    return get_token(data)
+
+
+def get_token_client_secret(client_id, client_secret):
+    data = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'grant_type': 'client_credentials'
+    }
+    return get_token(data)
+
+
+def get_token(data):
+    url = "https://q.daskeyboard.com/oauth/1.4/token"
+    headers = {"Content-Type": "application/json"}
+    r = requests.post(url, json=data, headers=headers)
+    return json.loads(r.content)
+
+
+class NoAuthenticationMethodException(Exception):
+    pass
--- pyq/pyq.py
+++ pyq/pyq.py
@@ -1,12 +1,26 @@
 import argparse
+import getpass
+import os
+import pprint
+
+from authentication import authenticate
 
 
 def auth(args):
-    print("hello auth")
+    token_dict = authenticate(args.email, args.password, args.client_id, args.client_secret)
+    pprint.pprint(token_dict)
 
 
 def signal(args):
     print("hello signal")
+
+
+class PasswordPromptAction(argparse.Action):
+    def __call__(self, parser, args, value, option_string=None):
+        if value is not None:
+            setattr(args, self.dest, value)
+        else:
+            setattr(args, self.dest, getpass.getpass())
 
 
 def main():
@@ -15,6 +29,16 @@ def main():
 
     auth_parser = subparsers.add_parser('auth', help='auth help')
     auth_parser.set_defaults(func=auth)
+
+    auth_parser.add_argument("--client_id", dest='client_id', default=os.getenv("PYQ_CLIENT_ID"),
+                             help="client id")
+    auth_parser.add_argument("--client_secret", dest='client_secret', default=os.getenv("PYQ_CLIENT_SECRET"),
+                             help="client secret")
+    auth_parser.add_argument("--email", dest='email', default=os.getenv("PYQ_EMAIL"),
+                             help="email")
+    auth_parser.add_argument("--password", dest="password", default=os.getenv("PYQ_PASSWORD"), nargs='?',
+                             action=PasswordPromptAction,
+                             help="password")
 
     signal_parser = subparsers.add_parser('signal', help='signal help')
     signal_parser.set_defaults(func=signal)
